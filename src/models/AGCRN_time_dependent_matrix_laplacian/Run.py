4527a7ae08c61900b25eb882854a66d4cbd2745a
palaceparis <palaceparisg@outlook.com>
2023-11-04 13:18:37 +0800

    1

--- src/models/AGCRN_time_dependent_matrix_laplacian/Run.py
+++ src/models/AGCRN_time_dependent_matrix_laplacian/Run.py
@@ -18,224 +18,119 @@ logging.basicConfig(level=logging.INFO)
 
 
 def objective_function(tuned_args: DictConfig, args: DictConfig) -> float:
-    if not args.hyperopt_tuning:
-        # Random seed
-        random.seed(args.seed)
-        torch.cuda.cudnn_enabled = False
-        torch.backends.cudnn.deterministic = True
-        np.random.seed(args.seed)
-        torch.manual_seed(args.seed)
-        torch.cuda.manual_seed(args.seed)
+    # Random seed
+    random.seed(args.seed)
+    torch.cuda.cudnn_enabled = False
+    torch.backends.cudnn.deterministic = True
+    np.random.seed(args.seed)
+    torch.manual_seed(args.seed)
+    torch.cuda.manual_seed(args.seed)
 
-        # Device
-        if torch.cuda.is_available():
-            torch.cuda.set_device(int(args.device[5]))
+    # Device
+    if torch.cuda.is_available():
+        torch.cuda.set_device(int(args.device[5]))
+    else:
+        args.device = "cpu"
+
+    print(args.device)
+
+    # Logging
+    logging.info(f"Embed Dimension: {args.embed_dim}")
+    logging.info(f"Number of Layers: {args.num_layers}")
+    logging.info(f"RNN Units: {args.rnn_units}")
+    logging.info(f"Chebyshev Polynomial Order: {args.cheb_k}")
+    logging.info(f"Initial Learning Rate: {args.lr_init}")
+    # logging.info(f"Number of Heads: {args.num_heads}")
+    logging.info(f"Hidden Dimension of Node: {args.hidden_dim_node}")
+    logging.info(f"Number of Layers of Node: {args.num_layers_node}")
+    # logging.info(f"Input Dimension: {args.input_dim}")
+
+    # Number of parameters
+    model = Network(args)
+    model = model.to(args.device)
+    for p in model.parameters():
+        if p.dim() > 1:
+            nn.init.xavier_uniform_(p)
         else:
-            args.device = "cpu"
+            nn.init.uniform_(p)
 
-        print(args.device)
+    logging.info("Model parameters:")
+    for name, param in model.named_parameters():
+        logging.info(f"{name}: {param.shape}")
+    logging.info(
+        f"Total number of parameters: {sum(p.numel() for p in model.parameters())}"
+    )
 
-        # Logging
-        logging.info(f"Embed Dimension: {args.embed_dim}")
-        logging.info(f"Number of Layers: {args.num_layers}")
-        logging.info(f"RNN Units: {args.rnn_units}")
-        logging.info(f"Chebyshev Polynomial Order: {args.cheb_k}")
-        logging.info(f"Initial Learning Rate: {args.lr_init}")
-        # logging.info(f"Number of Heads: {args.num_heads}")
-        logging.info(f"Hidden Dimension of Node: {args.hidden_dim_node}")
-        logging.info(f"Number of Layers of Node: {args.num_layers_node}")
-        # logging.info(f"Input Dimension: {args.input_dim}")
+    # load dataset
+    (
+        train_loader,
+        val_loader,
+        test_loader,
+        scaler,
+    ) = get_dataloader(
+        args,
+        normalizer=args.normalizer,
+        single=True,
+    )
 
-        # Number of parameters
-        model = Network(args)
-        model = model.to(args.device)
-        for p in model.parameters():
-            if p.dim() > 1:
-                nn.init.xavier_uniform_(p)
-            else:
-                nn.init.uniform_(p)
+    # init loss function, optimizer
+    loss = torch.nn.MSELoss().to(args.device)
+    optimizer = torch.optim.Adam(
+        params=model.parameters(),
+        lr=args.lr_init,
+        eps=1.0e-8,
+        weight_decay=0,
+        amsgrad=False,
+    )
 
-        logging.info("Model parameters:")
-        for name, param in model.named_parameters():
-            logging.info(f"{name}: {param.shape}")
-        logging.info(
-            f"Total number of parameters: {sum(p.numel() for p in model.parameters())}"
+    # learning rate decay
+    lr_scheduler = None
+    if args.lr_decay:
+        print("Applying learning rate decay.")
+        lr_decay_steps = [int(i) for i in list(args.lr_decay_step.split(","))]
+        lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
+            optimizer=optimizer, milestones=lr_decay_steps, gamma=args.lr_decay_rate
         )
 
-        # load dataset
-        (
+    if args.saved_model_path is not None:
+        logging.info("Loading saved model from {}".format(args.saved_model_path))
+        model.load_state_dict(torch.load(args.saved_model_path))
+        model.to(args.device)
+        # testing the model
+        logging.info("Testing the model.")
+
+        trainer = Trainer(
+            model,
+            loss,
+            optimizer,
             train_loader,
             val_loader,
             test_loader,
             scaler,
-        ) = get_dataloader(
             args,
-            normalizer=args.normalizer,
-            single=True,
+            lr_scheduler=lr_scheduler,
         )
 
-        # init loss function, optimizer
-        loss = torch.nn.MSELoss().to(args.device)
-        optimizer = torch.optim.Adam(
-            params=model.parameters(),
-            lr=args.lr_init,
-            eps=1.0e-8,
-            weight_decay=0,
-            amsgrad=False,
+        test_loss = trainer.test(
+            model, trainer.args, trainer.test_loader, trainer.scaler, trainer.logger
         )
-
-        # learning rate decay
-        lr_scheduler = None
-        if args.lr_decay:
-            print("Applying learning rate decay.")
-            lr_decay_steps = [int(i) for i in list(args.lr_decay_step.split(","))]
-            lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
-                optimizer=optimizer, milestones=lr_decay_steps, gamma=args.lr_decay_rate
-            )
-
-        if args.saved_model_path is not None:
-            logging.info("Loading saved model from {}".format(args.saved_model_path))
-            model.load_state_dict(torch.load(args.saved_model_path))
-            model.to(args.device)
-            # testing the model
-            logging.info("Testing the model.")
-
-            trainer = Trainer(
-                model,
-                loss,
-                optimizer,
-                train_loader,
-                val_loader,
-                test_loader,
-                scaler,
-                args,
-                lr_scheduler=lr_scheduler,
-            )
-
-            test_loss = trainer.test(
-                model, trainer.args, trainer.test_loader, trainer.scaler, trainer.logger
-            )
-            return {"loss": test_loss, "status": STATUS_OK}
-        else:
-            # start training
-            trainer = Trainer(
-                model,
-                loss,
-                optimizer,
-                train_loader,
-                val_loader,
-                test_loader,
-                scaler,
-                args,
-                lr_scheduler=lr_scheduler,
-            )
-            best_loss = trainer.train()
-
-            return {"loss": best_loss, "status": STATUS_OK}
-
+        return {"loss": test_loss, "status": STATUS_OK}
     else:
-        try:
-            # Random seed
-            random.seed(args.seed)
-            torch.cuda.cudnn_enabled = False
-            torch.backends.cudnn.deterministic = True
-            np.random.seed(args.seed)
-            torch.manual_seed(args.seed)
-            torch.cuda.manual_seed(args.seed)
+        # start training
+        trainer = Trainer(
+            model,
+            loss,
+            optimizer,
+            train_loader,
+            val_loader,
+            test_loader,
+            scaler,
+            args,
+            lr_scheduler=lr_scheduler,
+        )
+        best_loss = trainer.train()
 
-            # Device
-            if torch.cuda.is_available():
-                torch.cuda.set_device(int(args.device[5]))
-            else:
-                args.device = "cpu"
-
-            args.embed_dim = int(tuned_args["embed_dim"])
-            args.num_layers = int(tuned_args["num_layers"])
-            args.rnn_units = int(tuned_args["rnn_units"])
-            # args.cheb_k = int(tuned_args["cheb_k"])
-            args.lr_init = tuned_args["lr_init"]
-            # args.num_heads = int(tuned_args["num_heads"])
-            args.hidden_dim_node = int(tuned_args["hidden_dim_node"])
-            args.num_layers_node = int(tuned_args["num_layers_node"])
-            # args.input_dim = int(tuned_args["input_dim"])
-
-            logging.info(f"Embed Dimension: {args.embed_dim}")
-            logging.info(f"Number of Layers: {args.num_layers}")
-            logging.info(f"RNN Units: {args.rnn_units}")
-            # logging.info(f"Chebyshev Polynomial Order: {args.cheb_k}")
-            logging.info(f"Initial Learning Rate: {args.lr_init}")
-            # logging.info(f"Number of Heads: {args.num_heads}")
-            logging.info(f"Hidden Dimension of Node: {args.hidden_dim_node}")
-            logging.info(f"Number of Layers of Node: {args.num_layers_node}")
-            # logging.info(f"Input Dimension: {args.input_dim}")
-
-            # Number of parameters
-            model = Network(args)
-            model = model.to(args.device)
-            for p in model.parameters():
-                if p.dim() > 1:
-                    nn.init.xavier_uniform_(p)
-                else:
-                    nn.init.uniform_(p)
-
-            logging.info("Model parameters:")
-            for name, param in model.named_parameters():
-                logging.info(f"{name}: {param.shape}")
-            logging.info(
-                f"Total number of parameters: {sum(p.numel() for p in model.parameters())}"
-            )
-
-            # load dataset
-            (
-                train_loader,
-                val_loader,
-                test_loader,
-                scaler,
-            ) = get_dataloader(
-                args,
-                normalizer=args.normalizer,
-                single=True,
-            )
-
-            # init loss function, optimizer
-            loss = torch.nn.MSELoss().to(args.device)
-            optimizer = torch.optim.Adam(
-                params=model.parameters(),
-                lr=args.lr_init,
-                eps=1.0e-8,
-                weight_decay=0,
-                amsgrad=False,
-            )
-
-            # learning rate decay
-            lr_scheduler = None
-            if args.lr_decay:
-                print("Applying learning rate decay.")
-                lr_decay_steps = [int(i) for i in list(args.lr_decay_step.split(","))]
-                lr_scheduler = torch.optim.lr_scheduler.MultiStepLR(
-                    optimizer=optimizer,
-                    milestones=lr_decay_steps,
-                    gamma=args.lr_decay_rate,
-                )
-
-            # start training
-            trainer = Trainer(
-                model,
-                loss,
-                optimizer,
-                train_loader,
-                val_loader,
-                test_loader,
-                scaler,
-                args,
-                lr_scheduler=lr_scheduler,
-            )
-            best_loss = trainer.train()
-            return {"loss": best_loss, "status": STATUS_OK}
-
-        except Exception as e:
-            logging.error(f"Error occurred: {str(e)}")
-            return {"loss": float("inf"), "status": STATUS_OK}
+        return {"loss": best_loss, "status": STATUS_OK}
 
 
 @hydra.main(
